18b5a3f1cf145748eacede6d3b5142fd3a51e147
unknown <evan.mena13@gmail.com>
2018-04-16 11:42:22 -0400

    Added custom analysis script to compare a wordlist to full text, returns 4 tuples of (word, count)

---
+++ Analysis/wordCountOnList.py
@@ -0,0 +1,26 @@
+import sys
+
+import re
+
+def main(listPath, textPath):
+    wordList = open(listPath, "r").read().split("\n")[:-1]
+    wordDict = {}
+    for word in wordList:
+        wordDict[word] = 0
+
+
+    text = open(textPath, "r").read()
+    textWordList = re.split('\W+', text)
+
+
+    for word in textWordList:
+        print(word)
+        if word.lower() in wordList:
+            wordDict[word.lower()] += 1
+
+    print("Top 4 words found and number of occurances")
+    top4 = sorted(wordDict.items(), key = lambda x:x[1])
+    print(top4[-4:])
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
